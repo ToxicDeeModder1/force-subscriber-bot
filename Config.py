519f2931b41966ccf7c4ab5fc4e4b84514b3b886
Toxic Dee Modder <90701056+ToxicDeeModder1@users.noreply.github.com>
2021-09-18 12:57:59 +0200

    Update Config.py

--- Config.py
+++ Config.py
@@ -8,10 +8,10 @@ class Config():
     APP_ID = os.environ.get("APP_ID", 6)
     API_HASH = os.environ.get("API_HASH", None)
   else:
-    BOT_TOKEN = ""
+    BOT_TOKEN = "2029835371:AAHzwWfuqwA4rf-wbaBMC4CfZuveKRfegFQ"
     DATABASE_URL = ""
-    APP_ID = ""
-    API_HASH = ""
+    APP_ID = "8522555"
+    API_HASH = "9e0b9e90b854c23fe89ee57b0a75ff32"
 
 
 class Messages():
@@ -20,7 +20,7 @@ class Messages():
 
         "**Force Subscribers**\nForce group members to join a specific channel before sending messages in the group.\n\n**Setup**\n**Step 1.** __Add me in a group(in which you are creator of the group) as admin.__\n**Step 2.** __Send__ `/ForceSubscribe {your channel username}`\n**Step 3.** __Add me to your channel as admin.__\n\n`All set ! I mute users who didn't joined your channel and ask them to join channel and unmute themself.`\n\n**Commands**\n\n/ForceSubscribe { off / no / disable} - To stop force subscriber\n\n/ForceSubscribe {Channel Username} - Set the Channel\n\n/ForceSubscribe - Get current Status",
         
-        "**Developed by @admin🇮🇳**\n\nPowered by @admin🇮🇳\nThanks to - @PyroGram @HasibulKobir"
+        "**Developed by @ToxicDeeModderr🇮🇳**\n\nPowered by @TheBotsWorldChannel"
       ]
 
       START_MSG = "Hey [{}](tg://user?id={})\nI am a multifunctional group manager bot.\nLearn more at /help"
